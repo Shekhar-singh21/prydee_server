c6be7e30512182dc0aa544cc56f1aa019271c1da
shekhar <shekhar21k@gmail.com>
2024-01-14 15:54:43 +0530

    ready to be deployed

---
+++ src/controllers/bannerController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { Banner } from '../models';
+
+export const index = async (req: Request, res: Response) => {
+  try {
+    const banners = await Banner.find();
+    res.status(200).json({ data: banners });
+  } catch (error) {
+    console.log("test...error",error);
+    
+    res.status(500).json({ message: 'Error in getting banners' });
+  }
+};
